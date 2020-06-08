e792f75f1b0601370324f79ec7ecd94c169891d8
Diego Rojas <diegorojas431@gmail.com>
2020-06-08 17:07:00 -0500

    Múltiples peticiones a un API con Callbacks

--- src/callback/Challenge.js
+++ src/callback/Challenge.js
@@ -8,6 +8,7 @@
 
 //Guardamos en una variable el requerimiento de node xmlhttprequest
 let XMLHtmlRequest = require ('xmlhttprequest').XMLHttpRequest;
+let API = `https://rickandmortyapi.com/api/character/`;
 
 // Aqui, crearemos una función que traiga la informacion desde la API que queramos utilizar
 function fetchData(url_api, callBack) {
@@ -42,3 +43,18 @@ function fetchData(url_api, callBack) {
   // Se envia la solicitud
   xhttp.send();
 }
+
+/* --------------------------Callback-------------------------- */
+
+fetchData(API, function (error1, data1 ) {
+  if(error1) return console.error(error1);
+  fetchData(API +data1.results[0].id, function (error2, data2) {
+    if(error2) return console.error(error2);
+    fetchData(data2.origin.url, function (error3, data3) {
+      if(error3) return console.error(error3)
+      console.log(data1.info.count);
+      console.log(data2.name);
+      console.log(data3.dimension);
+    })
+  })
+})
